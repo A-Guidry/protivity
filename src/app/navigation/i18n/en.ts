245c0471bf33a8eb7e50bc06e597917f9f40cc21
Anthony <anthonyguidry1@me.com>
2019-10-11 21:20:27 -0700

    first

---
+++ src/app/navigation/i18n/en.ts
@@ -0,0 +1,21 @@
+export const locale = {
+    lang: 'en',
+    data: {
+        'NAV': {
+            'APPLICATIONS': 'Applications',
+            'DASHBOARDS'  : 'Dashboards',
+            'CALENDAR'    : 'Calendar',
+            'ACADEMY'     : 'Academy',
+            'MAIL'        : {
+                'TITLE': 'Mail',
+                'BADGE': '15'
+            },
+            
+            'CHAT'        : 'Chat',
+            'FILE_MANAGER': 'File Manager',
+            'CONTACTS'    : 'Contacts',
+            'TODO'        : 'Tasks',
+            'SCRUMBOARD'  : 'Projects'
+        }
+    }
+};
